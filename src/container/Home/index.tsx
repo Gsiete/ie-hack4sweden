aaaec49add1aff914c4545105abf6f272d310fa3
Gabriel Poussif <gpoussif@gmail.com>
2020-11-21 18:33:14 +0100

    ADD: hooks to fetch and submit data and an implementation sample

--- src/container/Home/index.tsx
+++ src/container/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import logo from '../../logo.svg';
-import { AuthContext } from '../AuthProvider';
+import AuthContext from '../AuthProvider/context';
 import { firestore } from '../../firebase';
 
 function Home() {
