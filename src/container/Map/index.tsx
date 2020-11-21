007bfd8a0a2fed22e650760a37d9238b8249d654
Gabriel Poussif <gpoussif@gmail.com>
2020-11-21 15:29:53 +0100

    ADD: put map component in /map

--- src/container/Map/index.tsx
+++ src/container/Map/index.tsx
@@ -2,30 +2,13 @@ import React from 'react';
 
 import logo from '../../logo.svg';
 import '../../App.css';
-import { AuthContext } from '../AuthProvider';
-import { firestore } from '../../firebase';
+import Map from '../../components/Map';
 
 function Home() {
-  const user = React.useContext(AuthContext);
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-      {user?.uid && (
-        <button onClick={() => firestore.collection('test').add({ userId: user?.uid, data: `test ${new Date().toJSON()}` })}>
-          Test Fs
-        </button>
-      )}
+      <Map />
     </header>
   );
 }
