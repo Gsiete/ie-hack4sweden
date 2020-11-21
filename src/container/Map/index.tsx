6214b66511f7b74cbdfeae771a13be65e415c25c
Pavel Schoffer <xschof00@gmail.com>
2020-11-21 23:42:22 +0100

    adds redirect

--- src/container/Map/index.tsx
+++ src/container/Map/index.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
 import logo from '../../logo.svg';
 import Map from '../../components/Map';
 import { useUserDataSubmitter } from '../../utils/hooks';
+import { Redirect } from 'react-router-dom';
 
 function Home() {
   const [isSending, setUserData] = useUserDataSubmitter()
+  const [redirectHome, setRedirectHome] = useState(false);
+  const handleSubmit = () => {
+    setUserData({ polygon: true })
+    setRedirectHome(true);
+  };
+
+  if (!isSending && redirectHome) {
+    return <Redirect to="/" />
+  }
+
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <Map />
-      <Button disabled={isSending} onClick={() => setUserData({ polygon: true })}>Submit!!!</Button>
+      <Button disabled={isSending} onClick={handleSubmit}>Submit!!!</Button>
     </header>
   );
 }
