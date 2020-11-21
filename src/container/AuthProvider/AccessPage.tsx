dd85e2f42cb3f04ad947c24581d24f716d2ddb26
Gabriel Poussif <gpoussif@gmail.com>
2020-11-21 17:22:09 +0100

    ADD: bootstrap

--- src/container/AuthProvider/AccessPage.tsx
+++ src/container/AuthProvider/AccessPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import firebase from 'firebase';
+import { Button, Container, Form } from 'react-bootstrap';
 
 import '../../App.css';
 import { auth } from '../../firebase';
@@ -21,19 +22,31 @@ const AccessPage: React.FC<Props> = ({ setUserInfo }) => {
       const provider = new firebase.auth.GoogleAuthProvider();
       auth.signInWithPopup(provider).then((result) => setUserInfo(result.user));
   }
-  console.log(formData)
+
   return (
-      <div>
-        <form>
-          <label htmlFor="email">Email</label>
-          <input name="email" id="email" onChange={setFromField} value={formData.email} />
-          <label htmlFor="password">Password</label>
-          <input name="password" id="password" onChange={setFromField} value={formData.password} type="password" />
-          <button type="button" disabled={!formData.email || !formData.password} onClick={signIn}>Sign-in</button>
-          <button type="button" disabled={!formData.email || !formData.password} onClick={signUp}>Sign-up</button>
-        </form>
-        <button type="button" onClick={loginWithGoogle}>Connect With Google</button>
-      </div>
+    <div className="App">
+      <Container>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" onChange={setFromField} value={formData.email} placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" onChange={setFromField} value={formData.password} placeholder="Enter password" />
+          </Form.Group>
+          <Button type="button" color="primary" disabled={!formData.email || !formData.password} onClick={signIn}>
+            Sign-in
+          </Button>
+          <Button type="button" color="secondary" disabled={!formData.email || !formData.password} onClick={signUp}>
+            Sign-up
+          </Button>
+        </Form>
+        <Button variant="outline-primary" type="button" onClick={loginWithGoogle} className="mt-3">
+          Connect With Google
+        </Button>
+      </Container>
+    </div>
   );
 }
 
