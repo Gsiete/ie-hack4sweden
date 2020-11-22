06edde61c882d0a1565dca7a0e35e30173084932
Pavel Schoffer <xschof00@gmail.com>
2020-11-22 08:15:30 +0100

    button

--- src/container/Map/index.tsx
+++ src/container/Map/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
-import logo from '../../logo.svg';
 import Map from '../../components/Map';
 import { useUserDataSubmitter } from '../../utils/hooks';
 import { Redirect } from 'react-router-dom';
@@ -18,7 +17,10 @@ function Home() {
     return <Redirect to="/" />
   }
 
-  return <Map/>;
+  return (<>
+    <Map />
+    <Button onClick={handleSubmit}>Save Polygon</Button>
+  </>);
 }
 
 export default Home;
